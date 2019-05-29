60bb4e4d4e2171d0220e194a56241b908a2081ae
Becky Phan <beckyphan@live.com>
2019-05-29 02:48:39 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,8 +5,9 @@ def roll_call_dwarves(list)
 end
 
 def summon_captain_planet(array)
-  array.collect |name| 
-  name.capitalize
+  array.collect do |name| 
+    puts name.capitalize + "!"
+  end
 end
 
 def long_planeteer_calls# code an argument here
