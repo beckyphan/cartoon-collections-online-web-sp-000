db4ddc8603caf0ac1771059ee8a007b59c3034bd
Becky Phan <beckyphan@live.com>
2019-05-29 02:46:33 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,7 @@
-def roll_call_dwarves(# code an argument h)
-  # Your code here
+def roll_call_dwarves(list)
+  list.each_with_index do |name, index|
+    puts "#{index} #{name}"
+  end
 end
 
 def summon_captain_planet# code an argument here
