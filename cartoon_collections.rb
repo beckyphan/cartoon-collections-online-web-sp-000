488d98a25d58a642d96bbc40048217b0eb208cd8
Becky Phan <beckyphan@live.com>
2019-05-29 03:14:16 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -24,5 +24,5 @@ end
 
 def find_the_cheese(array_of_strings)
   cheese_types = ["cheddar", "gouda", "camembert"]
-  array_of_strings.include?(cheese_types)
+  array_of_strings.detect(cheese_types)
 end
