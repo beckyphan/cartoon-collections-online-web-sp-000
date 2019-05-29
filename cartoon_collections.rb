0cbd6a916f7d07563f14100cdf8ac53919b98ee6
Becky Phan <beckyphan@live.com>
2019-05-29 03:17:57 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -24,11 +24,5 @@ end
 
 def find_the_cheese(array_of_strings)
   cheese_types = ["cheddar", "gouda", "camembert"]
-  cheese = []
-  array_of_strings.each do |item|
-    if item.include?(cheese_types) == true 
-      cheese.push(item)
-    else 
-    end
-  end
+  array_of_strings.find(cheese_types)
 end
