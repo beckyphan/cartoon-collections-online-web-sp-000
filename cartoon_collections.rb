3a9a7fe0990d96d2aa32826fcc4faefa7ac43798
Becky Phan <beckyphan@live.com>
2019-05-29 03:13:13 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -23,6 +23,6 @@ def long_planeteer_calls(array)
 end
 
 def find_the_cheese(array_of_strings)
-
   cheese_types = ["cheddar", "gouda", "camembert"]
+  if array_of_strings.include?(cheese_types)
 end
